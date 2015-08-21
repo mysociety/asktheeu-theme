c82d5bd94970b60998e3823b63f7edfc2a201484
Louise Crow <louise.crow@gmail.com>
2015-08-21 16:06:44 +0100

    2 space indentation.

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -1,9 +1,9 @@
 class ActionController::Base
-    before_filter :set_view_paths
+  before_filter :set_view_paths
 
-    def set_view_paths
-        self.prepend_view_path File.join(File.dirname(__FILE__), "views")
-    end
+  def set_view_paths
+    self.prepend_view_path File.join(File.dirname(__FILE__), "views")
+  end
 end
 
 # In order to have the theme lib/ folder ahead of the main app one,
@@ -29,11 +29,11 @@ $alaveteli_route_extensions << 'custom-routes.rb'
 
 # Prepend the asset directories in this theme to the asset path:
 ['stylesheets', 'images', 'javascripts'].each do |asset_type|
-    theme_asset_path = File.join(File.dirname(__FILE__),
-                                 '..',
-                                 'assets',
-                                 asset_type)
-    Rails.application.config.assets.paths.unshift theme_asset_path
+  theme_asset_path = File.join(File.dirname(__FILE__),
+                 '..',
+                 'assets',
+                 asset_type)
+  Rails.application.config.assets.paths.unshift theme_asset_path
 end
 
 Rails.application.config.assets.precompile.push 'asktheeu.css'
