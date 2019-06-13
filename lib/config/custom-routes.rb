6b83ab382453ff622aba1b550982cdc11229e209
Graeme Porteous <graeme@rgbp.co.uk>
2019-06-13 16:57:14 +0100

    Fix broken routes so application can now boot

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -3,9 +3,15 @@
 
 Rails.application.routes.draw do
   # Additional help pages
-  match '/help/help_out' => 'help#help_out', :as => 'help_help_out'
-  match '/help/right_to_know' => 'help#right_to_know', :as => 'help_right_to_know'
+  match '/help/help_out' => 'help#help_out',
+    :as => 'help_help_out',
+    :via => :get
+  match '/help/right_to_know' => 'help#right_to_know',
+    :as => 'help_right_to_know',
+    :via => :get
 
   # redirect the blog page to blog.asktheeu.org
-  match '/blog/' => redirect('http://blog.asktheeu.org/')
+  match '/blog/' => redirect('http://blog.asktheeu.org/'),
+    :as => :external_blog,
+    :via => :get
 end
