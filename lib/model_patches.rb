a63af910d68a62a8c1ea2a77c479b145e44061ab
Graeme Porteous <graeme@rgbp.co.uk>
2022-03-08 12:17:51 +0000

    Add patch to raw email for edge case issue
    Fixes issue where spaces are included before some email headers.
    
    See https://github.com/mysociety/alaveteli/issues/6853

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -153,4 +153,18 @@ Rails.configuration.to_prepare do
 
   end
 
+  RawEmail.class_eval do
+
+    alias original_data data
+
+    def data
+      original_data.sub(/
+        ^(Date: [^\n]+\n)
+        \s+(To: [^\n]+\n)
+        \s+(From: [^\n]+)
+      /x, '\1\2\3')
+    end
+
+  end
+
 end
